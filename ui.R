4b1ef06d91acc7cc22a1d55ffc2686f1ec258cab
Nicholas Henry Ssebirumbi <30862867+nssebirumbi@users.noreply.github.com>
2018-07-15 14:35:27 -0400

    Add files via upload
    user interface dashboard to be used

---
+++ ui.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+shinyUI(
+  dashboardPage(
+    dashboardHeader(title = "Mobile App Statistics"),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Home",icon = icon("home"),tabName = "home"
+                 
+        ),
+        menuItem("Import dataset",icon = icon("table"),tabName = "dataset"
+                 
+        ),
+        menuItem("Summary",tabName = "summary",icon = icon("list-alt")),
+        menuItem("Visualization",tabName = "visualization",icon=icon("bar-chart-o")),
+        menuItem("Help",tabName = "help",icon = icon("cog", lib = "glyphicon"))
+      )
+      
+      
+      
+    ),  
+    dashboardBody(
+      tabItems(
+        tabItem(tabName = "home"
+                
+        ),
+        tabItem(tabName = "dataset"
+                
+        ),
+        
+        tabItem(tabName = "visualization"
+                
+        ),
+        tabItem(tabName = "summary"
+                
+        ),
+        tabItem(tabName = "help" 
+                
+        )
+      )
+      
+      
+    )
+  )
+  
+)
