04d49a9db6924c362532be9b3aef92a80954d614
Martine3 <39619330+Martine3@users.noreply.github.com>
2018-07-17 16:28:17 +0300

    modified import dataset

--- ui.R
+++ ui.R
@@ -25,7 +25,68 @@ shinyUI(
         tabItem(tabName = "home"
                 
         ),
-        tabItem(tabName = "dataset"
+        tabItem(tabName = "dataset",
+                # Define UI for data upload app ----
+                ui <- fluidPage(
+                  
+                  # App title ----
+                  titlePanel("Uploading Files"),
+                  
+                  # Sidebar layout with input and output definitions ----
+                  sidebarLayout(
+                    
+                    # Sidebar panel for inputs ----
+                    sidebarPanel(
+                      
+                      # Input: Select a file ----
+                      fileInput("file1", "Choose CSV File",
+                                multiple = TRUE,
+                                accept = c(
+                                  ".csv")),
+                      
+                      uiOutput("selectfile"),
+                      
+                      # Horizontal line ----
+                      tags$hr(),
+                      
+                      # Input: Checkbox if file has header ----
+                      checkboxInput("header", "Header", TRUE),
+                      
+                      # Input: Select separator ----
+                      radioButtons("sep", "Separator",
+                                   choices = c(Comma = ",",
+                                               Semicolon = ";",
+                                               Tab = "\t"),
+                                   selected = ","),
+                      
+                      # Input: Select quotes ----
+                      radioButtons("quote", "Quote",
+                                   choices = c(None = "",
+                                               "Double Quote" = '"',
+                                               "Single Quote" = "'"),
+                                   selected = '"'),
+                      
+                      # Horizontal line ----
+                      tags$hr(),
+                      
+                      # Input: Select number of rows to display ----
+                      radioButtons("disp", "Display",
+                                   choices = c(Head = "head",
+                                               All = "all"),
+                                   selected = "head")
+                      
+                    ),
+                    
+                    # Main panel for displaying outputs ----
+                    mainPanel(
+                      
+                      # Output: Data file ----
+                      tableOutput("contents")
+                      
+                    )
+                    
+                  )
+                )
                 
         ),
         
