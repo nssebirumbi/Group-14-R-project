9e31b1c87f29e3246bf3db4a4e5c45c06f98171e
Nicholas Henry Ssebirumbi <30862867+nssebirumbi@users.noreply.github.com>
2018-07-26 11:14:23 +0300

    Update server.R
    final server script for the project which is well tested.

--- server.R
+++ server.R
@@ -1,6 +1,10 @@
 library(shiny)
 library(shinydashboard)
+library(syuzhet)
+library(tm)
 library(ggplot2)
+library(grid)
+library(dplyr)
 
 #line below increases the size of the file to be uploaded from the defalt 5MBS to 15MBS.
 options(shiny.maxRequestSize=15*1024^2)
@@ -15,122 +19,28 @@ shinyServer(function(input,output){
       selectInput("select","select",choices = input$file1$name)
     )
   })
-
-  #code to display  the loaded files
-  output$contents <- renderTable({
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
-    
+  data<-reactive({
     req(input$file1)
     
     df <- read.csv(input$file1$datapath[input$file1$name==input$select],
                    header = input$header,
                    sep = input$sep,
                    quote = input$quote)
+    return(df)
+  })
+
+  #code to display  the loaded files
+  output$contents <- renderTable({
     
     if(input$disp == "head") {
-      return(head(df))
+      return(head(data()))
     }
     else {
-      return(df)
+      return(data())
     }
     
   })
-
   
-})
-
-output$hist<-renderPlot({
-    
-    if(input$appDetails== "user_rating"){
-      #plot a bar graph showing the distribution of user ratings
-      return(ggplot(data = data1,aes(x=user_rating))+geom_histogram(bins = input$bins)+
-               
-               labs(title="A Histogram showing the distribution of user ratings",
-                    x="User Ratings",
-                    y="Number of apps"))
-    }
-    if(input$appDetails== "prime_genre"){
-      #plot a bar graph to show the distribution of app groups
-      return( ggplot(data=data1,aes(x=prime_genre))+
-                geom_bar()+
-                labs(title="A bar plot showing the distribution of app groups",
-                     x="App Group",
-                     y="Number of apps"
-                  
-                ))
-    }
-    if(input$appDetails=="size_bytes"){
-      return(
-        ggplot(data=data1,aes(x=size_bytes))+
-          geom_histogram(bins = input$bins)+
-          labs(title="A bar plot showing the distribution of app sizes",
-               x="size of apps in bytes",
-               y="Number of apps"
-               
-          )
-      )
-    }
-    if(input$appDetails=="price"){
-      return(
-        ggplot(data=data1,aes(x=as.factor(price)))+
-          geom_bar()+
-          labs(title="A bar plot showing the distribution of price",
-               x="Price",
-               y="Number of apps"
-            
-          )
-      )
-    }
-    if(input$appDetails=="lang.num"){
-      return(
-        ggplot(data=data1,aes(x=lang.num))+
-          geom_histogram(bins = input$bins)+
-          labs(title="A Histogram showing the distribution of Languages supported by each app",
-               x="Number of Languages supported",
-               y="Number of apps")
-      )
-    }
-    if(input$appDetails=="sup_devices.num"){
-      return(
-        ggplot(data = data1,aes(x=sup_devices.num))+
-          geom_histogram(bins=30)+
-          labs(title="Distribution of Number of devices supported",
-               x="Number of devices supported",
-               y="Number of apps")
-      )
-    }
-    else{
-      return(NULL)
-    }
-     output$price<-renderPlot({
-    if(input$price=="boxplot"){
-      return(
-        ggplot(data=data(),aes(x=as.factor(user_rating),y=price,fill=as.factor(user_rating)))+
-          theme_bw()+
-          geom_boxplot()+
-          labs(title="A box plot showing the how price affects user ratings",
-               x="user rating",
-               y="price")
-      )
-    }
-    if(input$price=="Regression"){
-      return(
-        ggplot(data=data(),aes(x=user_rating,y=price))+
-          geom_point()+
-          geom_smooth(method = lm, se=FALSE)+
-          labs(title="A scatter plot shhowing the correlation between price and user rating",
-               x="User rating",
-               y="Price of Apps"
-            
-          )
-        
-      )
-    }
-  })
- 
   output$hist<-renderPlot({
    # if(input$select=="AppleStore"){}
     
@@ -205,6 +115,31 @@ output$hist<-renderPlot({
     }
     
   })
+  output$price<-renderPlot({
+    if(input$price=="boxplot"){
+      return(
+        ggplot(data=data(),aes(x=as.factor(user_rating),y=price,fill=as.factor(user_rating)))+
+          theme_bw()+
+          geom_boxplot()+
+          labs(title="A box plot showing the how price affects user ratings",
+               x="user rating",
+               y="price")
+      )
+    }
+    if(input$price=="Regression"){
+      return(
+        ggplot(data=data(),aes(x=user_rating,y=price))+
+          geom_point()+
+          geom_smooth(method = lm, se=FALSE)+
+          labs(title="A scatter plot shhowing the correlation between price and user rating",
+               x="User rating",
+               y="Price of Apps"
+            
+          )
+        
+      )
+    }
+  })
   output$size<-renderPlot({
     if(input$size=="boxplot"){
       return(
@@ -230,37 +165,59 @@ output$hist<-renderPlot({
       )
     }
   })
- output$sentiment<-renderPlot({
-  myCopy <- data()$app_desc
-  #carryout sentiment mining using the get_nrc_sentiment()function #log the findings under a variable result
-  result_copy<- get_nrc_sentiment(as.character(myCopy))
-  #change result from a list to a data frame and transpose it 
-  result4<-data.frame(t(result_copy))
-  #rowSums computes column sums across rows for each level of a #grouping variable.
-  new_result1 <- data.frame(rowSums(result4))
-  #name rows and columns of the dataframe
-  names(new_result1)[1] <- "count"
-  new_result1 <- cbind("sentiment" = rownames(new_result1), new_result1)
-  rownames(new_result1) <- NULL
-  #plot the first 8 rows,the distinct emotions
-  qplot(sentiment, data=new_result1[1:8,], weight=count, geom="bar",fill=sentiment)+ggtitle("App description Sentiments")
-})
- output$polarity<-renderPlot({
-  myCopy <- data()$app_desc
-  #carryout sentiment mining using the get_nrc_sentiment()function #log the findings under a variable result
-  result_copy<- get_nrc_sentiment(as.character(myCopy))
-  #change result from a list to a data frame and transpose it 
-  result4<-data.frame(t(result_copy))
-  #rowSums computes column sums across rows for each level of a #grouping variable.
-  new_result1 <- data.frame(rowSums(result4))
-  #name rows and columns of the dataframe
-  names(new_result1)[1] <- "count"
-  new_result1 <- cbind("sentiment" = rownames(new_result1), new_result1)
-  rownames(new_result1) <- NULL
-  #plot the last 2 rows ,positive and negative
-  qplot(sentiment, data=new_result1[9:10,], weight=count, geom="bar",fill=sentiment)+ggtitle("App description Polarity")
-})
- output$groups<-renderPlot({
+  output$content<-renderPlot({
+    if(input$content=="boxplot"){
+      return(
+        ggplot(data=data(),aes(x=cont_rating,y=user_rating,fill=cont_rating))+
+          theme_bw()+
+          geom_boxplot()+
+          labs(title="A box plot showing the how size of apps affects user ratings",
+               x="content rating",
+               y="user rating")
+      )
+    }
+    if(input$content=="histogram"){
+      return(
+        ggplot(data=data(),aes(x=user_rating,fill=cont_rating))+
+          geom_histogram()+
+          labs(title="A Histogram shhowing the correlation between content Rating and user rating",
+               x="User rating",
+               y="Number of apps"
+               
+          )
+        
+      )
+    }
+    
+  })
+  output$languages<-renderPlot({
+    if(input$languages=="boxplot"){
+      return(
+        ggplot(data=data(),aes(x=as.factor(user_rating),y=lang.num,fill=as.factor(user_rating)))+
+          theme_bw()+
+          geom_boxplot()+
+          labs(title="A box plot showing the how size of apps affects user ratings",
+               x="User rating",
+               y="Number of languages supported by an App")
+      )
+    }
+    if(input$languages=="Regression"){
+      return(
+        ggplot(data=data(),aes(x=user_rating,y=lang.num))+
+          geom_point()+
+          geom_smooth(method = lm, se=FALSE)+
+          labs(title="A scatter plot shhowing the correlation between Number of languages supported by an App and user rating",
+               x="User rating",
+               y="No of languages supported by an App"
+               
+          )
+        
+      )
+    }
+    
+  })
+ 
+output$groups<-renderPlot({
   
   if(input$groups=="user_rating"){
     return(
@@ -284,22 +241,11 @@ output$hist<-renderPlot({
     )
     
   }
-  if(input$groups=="size_bytes"){
+  if(input$groups=="price"){
     return(
-      ggplot(data=data(),aes(x=prime_genre,y=size_bytes))+
+      ggplot(data=data(),aes(x=prime_genre,y=price))+
         theme_bw()+
         geom_boxplot()+
-        labs(title="Comparison of app Groups basing on size",
-             x="App Group",
-             y="Size ofApps in bytes")
-    )
-    
-  }
-  if(input$groups=="cont_rating"){
-    return(
-      ggplot(data=data(),aes(x=prime_genre,fill=cont_rating))+
-        theme_bw()+
-        geom_bar()+
         labs(title="Comparison of app Groups basing on price",
              x="App Group",
              y="price")
@@ -340,59 +286,7 @@ output$hist<-renderPlot({
   }
   
 })
- 
- output$screenshots<-renderPlot({
-  if(input$screenshots=="boxplot"){
-    return(
-      ggplot(data=data(),aes(x=as.factor(user_rating),y=ipadSc_urls.num,fill=as.factor(user_rating)))+
-        theme_bw()+
-        geom_boxplot()+
-        labs(title="A box plot showing the how Number of Screenshots affects user ratings",
-             x="user rating",
-             y="Number of Screenshots")
-    )
-  }
-  if(input$screenshots=="Regression"){
-    return(
-      ggplot(data=data(),aes(x=user_rating,y=ipadSc_urls.num))+
-        geom_point()+
-        geom_smooth(method = lm, se=FALSE)+
-        labs(title="A scatter plot shhowing the correlation between the Number of Screenshots and user rating",
-             x="User rating",
-             y="Number of Screen Shots"
-             
-        )
-      
-    )
-  }
-})
-  output$content<-renderPlot({
-    if(input$content=="boxplot"){
-      return(
-        ggplot(data=data(),aes(x=cont_rating,y=user_rating,fill=cont_rating))+
-          theme_bw()+
-          geom_boxplot()+
-          labs(title="A box plot showing the how size of apps affects user ratings",
-               x="User rating",
-               y="cont_rating")
-      )
-    }
-    if(input$content=="histogram"){
-      return(
-        ggplot(data=data(),aes(x=user_rating,fill=cont_rating))+
-          geom_histogram()+
-          labs(title="A Histogram shhowing the correlation between content Rating and user rating",
-               x="User rating",
-               y="content rating"
-               
-          )
-        
-      )
-    }
-    
-  })
- 
- output$sum_ary<-renderTable({
+output$sum_ary<-renderTable({
   if(input$columns=="user_rating"){
     return(
       as.array(summary(data()$user_rating))
@@ -432,8 +326,53 @@ output$hist<-renderPlot({
   
   
 })
- 
- output$table<-renderTable({
+output$versions<-renderPlot({
+  plot1 <-  ggplot(data = data(),aes(x=user_rating))+geom_freqpoly()+
+    labs(title="Distribution of User ratings for all app versions",
+         x="User ratings for all versions",
+         y="Number of apps")
+  
+  
+  plot2 <-  ggplot(data = data(),aes(x=user_rating_ver))+geom_freqpoly()+
+    labs(title="Distribution of User ratings for current app versions",
+         x="User ratings for current versions",
+         y="Number of apps")
+  
+  
+  grid.newpage()
+  grid.draw(rbind(ggplotGrob(plot1), ggplotGrob(plot2), size = "last"))
+})
+output$sentiment<-renderPlot({
+  myCopy <- data()$app_desc
+  #carryout sentiment mining using the get_nrc_sentiment()function #log the findings under a variable result
+  result_copy<- get_nrc_sentiment(as.character(myCopy))
+  #change result from a list to a data frame and transpose it 
+  result4<-data.frame(t(result_copy))
+  #rowSums computes column sums across rows for each level of a #grouping variable.
+  new_result1 <- data.frame(rowSums(result4))
+  #name rows and columns of the dataframe
+  names(new_result1)[1] <- "count"
+  new_result1 <- cbind("sentiment" = rownames(new_result1), new_result1)
+  rownames(new_result1) <- NULL
+  #plot the first 8 rows,the distinct emotions
+  qplot(sentiment, data=new_result1[1:8,], weight=count, geom="bar",fill=sentiment)+ggtitle("App description Sentiments")
+})
+output$polarity<-renderPlot({
+  myCopy <- data()$app_desc
+  #carryout sentiment mining using the get_nrc_sentiment()function #log the findings under a variable result
+  result_copy<- get_nrc_sentiment(as.character(myCopy))
+  #change result from a list to a data frame and transpose it 
+  result4<-data.frame(t(result_copy))
+  #rowSums computes column sums across rows for each level of a #grouping variable.
+  new_result1 <- data.frame(rowSums(result4))
+  #name rows and columns of the dataframe
+  names(new_result1)[1] <- "count"
+  new_result1 <- cbind("sentiment" = rownames(new_result1), new_result1)
+  rownames(new_result1) <- NULL
+  #plot the last 2 rows ,positive and negative
+  qplot(sentiment, data=new_result1[9:10,], weight=count, geom="bar",fill=sentiment)+ggtitle("App description Polarity")
+})
+output$table<-renderTable({
   if(input$table=="user_rating"){
     return(
       as.array(table(data()$user_rating))
@@ -478,4 +417,31 @@ output$hist<-renderPlot({
   
   
 })
-  })
+output$screenshots<-renderPlot({
+  if(input$screenshots=="boxplot"){
+    return(
+      ggplot(data=data(),aes(x=as.factor(user_rating),y=ipadSc_urls.num,fill=as.factor(user_rating)))+
+        theme_bw()+
+        geom_boxplot()+
+        labs(title="A box plot showing the how Number of Screenshots affects user ratings",
+             x="user rating",
+             y="Number of Screenshots")
+    )
+  }
+  if(input$screenshots=="Regression"){
+    return(
+      ggplot(data=data(),aes(x=user_rating,y=ipadSc_urls.num))+
+        geom_point()+
+        geom_smooth(method = lm, se=FALSE)+
+        labs(title="A scatter plot shhowing the correlation between the Number of Screenshots and user rating",
+             x="User rating",
+             y="Number of Screen Shots"
+             
+        )
+      
+    )
+  }
+})
+  
+})
+
